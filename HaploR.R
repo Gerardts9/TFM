69c645210f3f5e4a58cbbb0f3b9fcefc9e40b280
Gerardts9 <58481411+Gerardts9@users.noreply.github.com>
2020-09-14 15:05:49 +0200

    Update HaploR.R

--- HaploR.R
+++ HaploR.R
@@ -28,9 +28,3 @@ write.xlsx(x=PST, file="/Users/Usuario/Desktop/PST.haploR.xlsx")
 #PSF:
 PSF <- queryHaploreg(query=c("rs121918472","rs150611042"), ldThresh = 0.5)
 write.xlsx(x=PSF, file="/Users/Usuario/Desktop/PSF.haploR.xlsx")
-
-
-
-gsub("^([^:]*:[^:]*):.*$", "\\1", AT$MarkerName)
-/home/pferran/Others/sfwr/locuszoom/bin/locuszoom --metal LocusZoomAt.txt --build hg38 --pop EUR --source 1000G_Nov2014 --refsnp chr1:173914872 --flank 250kb --pvalcol P.value --ld-vcf /home/pferran/bdd/ref/ldref/proc/2imp/chr1.dose.vcf.gz
-chr1:173914872
